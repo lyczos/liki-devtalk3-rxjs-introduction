f1d8a0305a99dd1690fdd92ea617ad3c36fb4670
Michał Łyczko <lyczos@gmail.com>
2019-03-27 20:03:06 +0100

    add utils metods

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
-import { of } from 'rxjs';
+interface Joke {
+  id: number;
+  punchline: string;
+  setup: string;
+  type: string;
+}
 
-const observable = of('test');
-observable.subscribe(
-  (x: any) => logItem(x),
-  (error: any) => logItem('Error: ' + error),
-  () => logItem('Completed')
-);
+const apiEndpoint = 'http://localhost:3005';
 
-function logItem(val: any) {
-  const node = document.createElement('li');
-  const textnode = document.createTextNode(val);
-  node.appendChild(textnode);
-  document.getElementById('list').appendChild(node);
+function getJoke(): Promise<Joke> {
+  return fetch(apiEndpoint + '/random_joke', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json());
+}
+
+function createJokeNode(joke: Joke) {
+  const li = document.createElement('li');
+  const divSetup = document.createElement('div');
+  const divPunchline = document.createElement('div');
+
+  const setup = document.createTextNode(joke.setup);
+  const punchline = document.createTextNode(joke.punchline);
+
+  divSetup.appendChild(setup);
+  divPunchline.appendChild(punchline);
+
+  li.className = 'joke';
+  divSetup.className = 'joke-setup';
+  divPunchline.className = 'joke-punchline';
+
+  li.append(divSetup, divPunchline);
+
+  document.getElementById('list').appendChild(li);
 }
