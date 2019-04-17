1235af7714c188d150db1c5392a7daf8fdc804b8
Michał Łyczko <lyczos@gmail.com>
2019-04-17 12:26:45 +0200

    cleanup..

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import { fromEvent, timer } from 'rxjs';
-import { createJokeNode, getJoke } from './utils';
-import { concatMap, tap } from 'rxjs/operators';
-
 const rndJokeBtn = document.getElementById('rndJokeBtn');
 const jokeStreamBtn = document.getElementById('jokeStreamBtn');
