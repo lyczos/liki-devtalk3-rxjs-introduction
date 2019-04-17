90d0a181a51d8d19f6da912ab08d69aeac705355
Michał Łyczko <lyczos@gmail.com>
2019-04-17 12:05:38 +0200

    utils...

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,3 @@ import { concatMap, tap } from 'rxjs/operators';
 
 const rndJokeBtn = document.getElementById('rndJokeBtn');
 const jokeStreamBtn = document.getElementById('jokeStreamBtn');
-
-const rndJokeClick$ = fromEvent(rndJokeBtn, 'click').pipe(
-  tap(x => console.log('Tap debug', x))
-);
-
-const rndJokeClickSubscription = rndJokeClick$.subscribe(ev => {
-  console.log('ev', ev);
-  getJoke().subscribe(joke => createJokeNode(joke));
-});
-
-setTimeout(() => {
-  console.log('Unsubscribe...');
-  rndJokeClickSubscription.unsubscribe();
-}, 5000);
